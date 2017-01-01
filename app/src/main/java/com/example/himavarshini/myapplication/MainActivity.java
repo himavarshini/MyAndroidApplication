efdff8250fb39dfbfa0ba61a20058cc9031dc58c
himavarshini <himayarragangu@gmail.com>
2017-01-01 19:48:38 +0530

    Android Applicationwith basic features

---
+++ app/src/main/java/com/example/himavarshini/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.himavarshini.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+}
